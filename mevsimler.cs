5d17baa95963ffd8020cb45f10c8e0e988b068fb
Esma Dinler <101185632+esmadinler@users.noreply.github.com>
2023-01-09 21:04:34 +0300

    Mevsimler-Aylar

---
+++ mevsimler.cs
@@ -0,0 +1,21 @@
+string mevsim;
+mevsim = textBox1.Text;
+
+if (mevsim == "ilkbahar")
+MessageBox.Show(mevsim + " Mevsimindeki aylar Mart Nisan 
+Mayıs");
+
+else if (mevsim == "yaz")
+MessageBox.Show(mevsim + " Mevsimindeki aylar Haziran 
+Temmuz Ağustos");
+
+else if (mevsim == "sonbahar")
+MessageBox.Show(mevsim + " Mevsimindeki aylar Eylül Ekim 
+Kasım");
+
+else if (mevsim == "kış")
+MessageBox.Show(mevsim + " Mevsimindeki aylar Aralık Ocak 
+Şubat");
+
+else
+MessageBox.Show("Hatalı giriş yaptınız");
