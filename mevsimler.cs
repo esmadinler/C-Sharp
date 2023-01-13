3b752f246614cd12f1552aa77890dff99a9b5d4c
Esma Dinler <101185632+esmadinler@users.noreply.github.com>
2023-01-13 14:21:56 +0300

    Update mevsimler.cs

--- mevsimler.cs
+++ mevsimler.cs
@@ -1,21 +1,24 @@
-string mevsim;
-mevsim = textBox1.Text;
+private void textBox1_TextChanged(object sender, EventArgs e)
+{
+    string mevsim;
+    mevsim = textBox1.Text;
 
-if (mevsim == "ilkbahar")
-MessageBox.Show(mevsim + " Mevsimindeki aylar Mart Nisan 
-Mayıs");
+    if (mevsim == "ilkbahar")
+    {MessageBox.Show(mevsim + " Mevsimindeki aylar Mart Nisan 
+    Mayıs");}
 
-else if (mevsim == "yaz")
-MessageBox.Show(mevsim + " Mevsimindeki aylar Haziran 
-Temmuz Ağustos");
+    else if (mevsim == "yaz")
+    {MessageBox.Show(mevsim + " Mevsimindeki aylar Haziran 
+    Temmuz Ağustos");}
 
-else if (mevsim == "sonbahar")
-MessageBox.Show(mevsim + " Mevsimindeki aylar Eylül Ekim 
-Kasım");
+    else if (mevsim == "sonbahar")
+    {MessageBox.Show(mevsim + " Mevsimindeki aylar Eylül Ekim 
+    Kasım");}
 
-else if (mevsim == "kış")
-MessageBox.Show(mevsim + " Mevsimindeki aylar Aralık Ocak 
-Şubat");
+    else if (mevsim == "kış")
+    {MessageBox.Show(mevsim + " Mevsimindeki aylar Aralık Ocak 
+    Şubat");}
 
-else
-MessageBox.Show("Hatalı giriş yaptınız");
+    else
+    {MessageBox.Show("Hatalı giriş yaptınız");}
+}
